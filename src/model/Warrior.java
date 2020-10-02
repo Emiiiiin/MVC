7b36e6ed75cee76933cf131c8cd33fd73db8763c
bergrobi <bergrobi@kkg-eu.do>
2020-10-02 09:30:26 +0200

    zeug

--- src/model/Warrior.java
+++ src/model/Warrior.java
@@ -8,4 +8,22 @@ public abstract class Warrior extends FightingObject{
     public int getSpecialattack(){
         return specialattack;
     }
+    protected int SP;
+    protected int fleePower;
+    protected int evasion;
+    /*
+    SP is needed for performing special attacks
+    after performing a special attack a specific amount of SP is removed
+    all or some Characters regain some SP at the end of the round
+
+    fleePower is used to escape the battle (not boss fights)
+    methode run
+    your chance for escape increases temporarily after every (failed try)
+
+    evasion is the chance for the player to dodge an attack
+    methode doge (increases evasion greatly for one or two rounds)
+
+    method defense (increases Def)
+
+     */
 }
