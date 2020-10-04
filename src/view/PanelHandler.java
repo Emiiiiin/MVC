ccfcac35fde12fade4e323026ef4e918eb87f29c
Niclas <voelnicl@kkg-do.eu>
2020-10-04 10:51:00 +0200

    Radiobuttons richtig einprogrammiert

--- src/view/PanelHandler.java
+++ src/view/PanelHandler.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PanelHandler {
+    private int warrior = 0;
     private JPanel panel;
     private JRadioButton warrior4RadioButton;
     private JRadioButton warrior3RadioButton;
@@ -27,16 +28,48 @@ public class PanelHandler {
     }
 
     private void createButtons() {
-        ActionListener listener = new ActionListener() {
+        hunterRadioButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-
+            public void actionPerformed(ActionEvent e) {
+                archerRadioButton.setSelected(false);
+                warrior3RadioButton.setSelected(false);
+                warrior4RadioButton.setSelected(false);
+                warrior = 1;
             }
-        };
-        warrior4RadioButton.addActionListener(listener);
-        warrior3RadioButton.addActionListener(listener);
-        archerRadioButton.addActionListener(listener);
-        hunterRadioButton.addActionListener(listener);
+        });
+        archerRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hunterRadioButton.setSelected(false);
+                warrior3RadioButton.setSelected(false);
+                warrior4RadioButton.setSelected(false);
+                warrior = 2;
+            }
+        });
+        warrior3RadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hunterRadioButton.setSelected(false);
+                archerRadioButton.setSelected(false);
+                warrior4RadioButton.setSelected(false);
+                warrior = 3;
+            }
+        });
+        warrior4RadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hunterRadioButton.setSelected(false);
+                archerRadioButton.setSelected(false);
+                warrior3RadioButton.setSelected(false);
+                warrior = 4;
+            }
+        });
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mainController.chosenWarrior(warrior);
+            }
+        });
         ActionListener listener1 = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -47,7 +80,6 @@ public class PanelHandler {
         infoButton2.addActionListener(listener1);
         infoButton3.addActionListener(listener1);
         infoButton4.addActionListener(listener1);
-        button1.addActionListener(listener1);
     }
 
     private void updateGUI() {
