6fbd73c3840b8e4da6c5f1cb9d797e2597faec6a
bergrobi <bergrobi@kkg-do.eu>
2020-10-05 21:51:07 +0200

    Guard etc.

---
+++ src/model/Guard.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Guard extends Warrior {
+    public Guard(String name, int hp, int attack, int defense, boolean isCriticalHit, int isCriticalHitChance, int SP, int fleePower, int evasion) {
+        super(name, hp, attack, defense, isCriticalHit, isCriticalHitChance, SP, fleePower, evasion);
+    }
+}
